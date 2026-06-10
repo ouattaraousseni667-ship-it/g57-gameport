91045911935f325e44bf51a354b0d5e7f015072e
ouattaraousseni667-ship-it <ouattaraousseni667@gmail.com>
2026-06-10 21:34:10 +0000

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div style={{padding: 20, fontFamily: 'Arial'}}>
+      <h1>GameHub G57 ✅</h1>
+      <p>Build réussi boss !</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
